c84a5164ff56b43e4f384ae3404744f0917eca34
Stenstromen <kontakt@filipstenstrom.se>
2026-03-29 22:18:01 +0200

    depends on kyverno

--- outputs.tf
+++ outputs.tf
@@ -14,17 +14,17 @@ output "cluster_endpoint" {
   value       = kind_cluster.default.endpoint
 }
 
-output "nginx_namespace" {
-  description = "Namespace where nginx is deployed"
-  value       = kubernetes_namespace.nginx.metadata[0].name
-}
-
-output "nginx_service_url" {
-  description = "URL to access nginx service"
-  value       = "http://localhost:30080"
-}
-
-output "nginx_deployment_name" {
-  description = "Name of the nginx deployment"
-  value       = kubernetes_deployment.nginx.metadata[0].name
-}
+#output "nginx_namespace" {
+#  description = "Namespace where nginx is deployed"
+#  value       = kubernetes_namespace.nginx.metadata[0].name
+#}
+#
+#output "nginx_service_url" {
+#  description = "URL to access nginx service"
+#  value       = "http://localhost:30080"
+#}
+#
+#output "nginx_deployment_name" {
+#  description = "Name of the nginx deployment"
+#  value       = kubernetes_deployment.nginx.metadata[0].name
+#}
